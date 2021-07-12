8b873a3ca1a309d0239b696312c18017432bcce2
William Duong <altwpd@gmail.com>
2021-07-12 09:02:11 -0700

    Made a class for patients

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ Mon July 12, 2021
 wpduong@gmail.com
 
 """
+
+class Patient:
+	def __init__ (self, cwid, name, patientType):
+		"""Initialize class data members"""
+		self.cwid = cwid
+		self.name = name
+		self.patientType = patientType
+
+
+def main():
+	"""Main Function"""
+
+	
+
+
+main()

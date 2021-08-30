7883fb09b67c90d1f6de2c2730adc4136794397d
William Duong <altwpd@gmail.com>
2021-08-30 09:41:40 -0700

    Formatted the output

--- runScript.bat
+++ runScript.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-::This batch file helps streamline the process of^
-running the compliancy counter.
+::This batch file helps streamline the process of running the compliancy counter.
 TITLE Running the Compliancy Counter...
 ECHO Starting processes...
+ECHO.
 "C:\Users\zz-wduong\AppData\Local\Programs\Python\Python39\python.exe" ^
 "C:\Users\zz-wduong\Dropbox (CSU Fullerton)\PC\Desktop\Run Counter\main.py"
 PAUSE
